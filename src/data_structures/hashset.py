daf723797783f3bada59293944067f6b8b5234b9
therighteric <eric.r.rong@gmail.com>
2019-12-03 16:53:50 -0500

    Changed set to a dictionary, added intersection and union functionality - untested

--- src/data_structures/hashset.py
+++ src/data_structures/hashset.py
@@ -4,11 +4,28 @@ from data_structures.set_abc import Set
 class HashSet(Set):
 
     def __init__(self):
-        self.elements = set()
+        self.elements = {}
 
     def contains(self, x):
         return x in self.elements
 
     def insert(self, x):
-        self.elements.add(x)
+        if not contains(self, x):
+            self.elements[x] = 1
+        else:
+            self.elements[x] += 1
 
+    def intersect(self, other):
+        keyIntersect = set(self.keys()) & set(other.keys())
+        ret = {}
+        for i in keyIntersect:
+            ret[i] = self[i] + other[i]
+        return ret
+
+    def union(self, other):
+        keyUnion = self.keys().union(other.keys())
+        ret = {}
+        for i in keyUnion:
+            #Get value if exists, add 0 if it doesn't exist
+            ret[i] = self.get(i, 0) + other.get(i, 0)
+        return ret
