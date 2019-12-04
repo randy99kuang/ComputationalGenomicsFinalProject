f55b1d5cab7e9223272cec623ebe3955d718b2ae
randy99kuang <kuang.randy@gmail.com>
2019-12-04 15:07:59 -0500

    added more hiv data and added hashset unit tests

--- tests/test_hashset.py
+++ tests/test_hashset.py
@@ -1,4 +1,30 @@
-import unittest
-import src.data_structures.hashset
+import unittest, random
+from src.data_structures.hashset import HashSet
+
 
 class TestHashSet(unittest.TestCase):
+
+    def test_test(self):
+        self.assertTrue(True)
+        self.assertFalse(False)
+
+    def test_hashset(self):
+        s = HashSet()
+        tempList = []
+        for i in range(1000):
+            tempList.append(i)
+
+        random.shuffle(tempList)
+        for i in tempList:
+            s.insert(i)
+
+        for i in range(1000):
+            self.assertTrue(s.contains(i))
+
+        for i in range(-1, -1000, -1):
+            self.assertFalse(s.contains(i))
+
+
+
+
+
