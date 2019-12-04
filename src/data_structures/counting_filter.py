8b90fb36821a43f0a6f2e130b375354f9129662a
Amritpal Singh <amritsunner29@gmail.com>
2019-12-04 15:46:12 -0500

    counting filter

--- src/data_structures/counting_filter.py
+++ src/data_structures/counting_filter.py
@@ -11,22 +11,45 @@ class counting_filter(object):
     def __init__(self, genome):
         self.items = [0] * len(genome)
 
-    def add(self, item):
+    def insert(self, item):
         bits = mask(item)
         for index, bit in enumerate(bits):
             if bit == '1':
                 self.items[index] += 1
 
-    def query(self, item):
+    def contains(self, item):
         bits = mask(item)
         for index, bit in enumerate(bits):
             if bit == '1' and self.items[index] == 0:
                 return False
         return True
 
+    def getCount(self, x):
+        b = contains(x)
+        if x == False:
+            return 0
+        bits = mask(x)
+        low = float("inf")
+        for index, bit in enumerate(bits):
+            if bit == '1' and self.items[index] < low:
+                low = self.items[index]
+        return low
+
+    def intersect(self, other):
+        for index in range(len(self.items)):
+            if self.items[index] < other[index]:
+                continue
+            elif other[index] < self.items[index]:
+                self.items[index] = other[index]
+        return self.items
+
+    def union(self, other):
+        for index in range(len(self.items)):
+            self.items[index] += other[index]
+        return self.items
+
     def remove(self, item):
         bits = mask(item)
         for index, bit in enumerate(bits):
             if bit == '1' and self.items[index]:
                 self.items[index] -= 1
-    
