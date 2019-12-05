defdf67fcea01ead9ed7330366c37c581dbec584
dkasima1@jhu.edu <dkasima1@jhu.edu>
2019-12-05 17:44:09 -0500

    changed import statement

--- src/analysis/main.py
+++ src/analysis/main.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(1, '../../')
-from src.analysis.data_analysis import *
+from src.analysis.data_analysis_hiv import *
 
 """
 This file is just used to run methods in the data_analysis.py file
