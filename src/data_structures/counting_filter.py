0c5d14e65470bce97e3d16073cad6cacccba61fa
Amritpal Singh <amritsunner29@gmail.com>
2019-12-05 16:31:36 -0500

    counting filter

--- src/data_structures/counting_filter.py
+++ src/data_structures/counting_filter.py
@@ -1,58 +1,195 @@
-def hashfn(item):
-    h = hash(item)
-    return (1 << (h%64)) | (1 << (h/64%64))
+import math
+import mmh3
+from array import array
+from src.data_structures.set_abc import Set
 
 
-def mask(val):
-    return bin(hashfn(val))[2:]
+class CountingFilter(Set):
+    """
+    Bloom Filter implementation that uses bitarray internally as well as hash functions from mmh3
+    """
+
+    def __init__(self, items_count, fp_prob):
+        """
+        items_count : int
+            Number of items expected to be stored in bloom filter
+        fp_prob : float
+            False Positive probability in decimal
+        """
+
+        # False positive probability in decimal
+        self.fp_prob = fp_prob
+
+        # Size of bit array to use
+        self.size = self.get_size(items_count, fp_prob)
+
+        # number of hash functions to use
+        self.hash_count = self.get_hash_count(self.size, items_count)
+
+        # Bit array of given size
+        self.bit_array = [0] * self.size
 
 
-class counting_filter(object):
-    def __init__(self, genome):
-        if genome == 'hiv':
-            self.items = [0] * 143776
-        elif genome == 'ecoli':
-            self.items = [0]
+    def getBitSize(self):
+        return self.size
+
+    def getHashCount(self):
+        return self.hash_count
 
     def insert(self, item):
-        bits = mask(item)
-        for index, bit in enumerate(bits):
-            if bit == '1':
-                self.items[index] += 1
+        """
+        Add an item in the filter
+        """
+        digests = []
+        for i in range(self.hash_count):
+            # create digest for given item.
+            # i work as seed to mmh3.hash() function
+            # With different seed, digest created is different
+            digest = mmh3.hash(item, i) % self.size
+            digests.append(digest)
+
+            # Add to each element in bit_array
+        self.bit_array = [x+1 for x in self.bit_array[digests]]
 
     def contains(self, item):
-        bits = mask(item)
-        for index, bit in enumerate(bits):
-            if bit == '1' and self.items[index] == 0:
+        """
+        Check for existence of an item in filter
+        """
+        for i in range(self.hash_count):
+            digest = mmh3.hash(item, i) % self.size
+            if self.bit_array[digest] == 0:
+                # if any of bit is False then,its not present
+                # in filter
+                # else there is probability that it exist
                 return False
         return True
 
-    def getCount(self, x):
-        b = contains(x)
-        if x == False:
-            return 0
-        bits = mask(x)
-        low = float("inf")
-        for index, bit in enumerate(bits):
-            if bit == '1' and self.items[index] < low:
-                low = self.items[index]
-        return low
+    def getBitArray(self):
+        return self.bit_array
 
-    def intersect(self, other):
-        for index in range(len(self.items)):
-            if self.items[index] < other[index]:
+    def union(self, b):
+        """
+        b  :  bloom_filter to union with
+
+        unions self and b, and stores the union in self
+        b will be automatically discarded by garbage collector
+        """
+
+        for index in range(self.size):
+            self.bit_array[index] += b[index]
+
+        return self
+
+    def intersection(self, b):
+        """
+        b  :  bloom_filter to intersect with
+
+        intersects self and b, and stores the intersection in self
+        b will be automatically discarded by garbage collector
+        """
+
+        for index in range(self.size):
+            if self.bit_array[index] < b[index]:
                 continue
-            elif other[index] < self.items[index]:
-                self.items[index] = other[index]
-        return self.items
+            elif b[index] < self.bit_array[index]:
+                self.bit_array[index] = b[index]
+        return self
 
-    def union(self, other):
-        for index in range(len(self.items)):
-            self.items[index] += other[index]
-        return self.items
+    @classmethod
+    def get_size(self, n, p):
+        """
+        n : int
+            number of items expected to be stored in filter
+        p : float
+            False Positive probability in decimal
 
-    def remove(self, item):
-        bits = mask(item)
-        for index, bit in enumerate(bits):
-            if bit == '1' and self.items[index]:
-                self.items[index] -= 1
+        Return the optimal size of the bit array given the expected number of elements and the false positive rate.
+        From class, we are given that for false positive rate follows the formula:
+        p = (1 - e^(-hn/m))^h
+
+        From class, we also know that the optimal number of hash functions is given by the formula:
+        h = (m/n) * ln(2)
+
+        If we substitute this value of h into the previous equation and then isolate m, we get the equation below. Note
+        that a formal proof of this will be given in the paper we turn in.
+        m = -(n * ln(p)) / (ln(2)^2)
+        """
+
+        m = -(n * math.log(p)) / (math.log(2) ** 2)
+        return int(m)
+
+    @classmethod
+    def get_hash_count(self, m, n):
+        """
+        m : int
+            size of bit array
+        n : int
+            number of items expected to be stored in filter
+
+        Return the number of hash functions needed to minimize the false positive rate.
+        From class, we are given that the formula for this is
+        h = (m/n) * ln(2)
+        """
+
+        h = (m / n) * math.log(2)
+        return int(h)
+
+
+# def hashfn(item):
+#     h = hash(item)
+#     return (1 << (h%64)) | (1 << (h/64%64))
+
+
+# def mask(val):
+#     return bin(hashfn(val))[2:]
+
+
+# class counting_filter(object):
+#     def __init__(self, genome):
+#         if genome == 'hiv':
+#             self.items = [0] * 143776
+#         elif genome == 'ecoli':
+#             self.items = [0]
+
+#     def insert(self, item):
+#         bits = mask(item)
+#         for index, bit in enumerate(bits):
+#             if bit == '1':
+#                 self.items[index] += 1
+
+#     def contains(self, item):
+#         bits = mask(item)
+#         for index, bit in enumerate(bits):
+#             if bit == '1' and self.items[index] == 0:
+#                 return False
+#         return True
+
+#     def getCount(self, x):
+#         b = contains(x)
+#         if x == False:
+#             return 0
+#         bits = mask(x)
+#         low = float("inf")
+#         for index, bit in enumerate(bits):
+#             if bit == '1' and self.items[index] < low:
+#                 low = self.items[index]
+#         return low
+
+#     def intersect(self, other):
+#         for index in range(len(self.items)):
+#             if self.items[index] < other[index]:
+#                 continue
+#             elif other[index] < self.items[index]:
+#                 self.items[index] = other[index]
+#         return self.items
+
+#     def union(self, other):
+#         for index in range(len(self.items)):
+#             self.items[index] += other[index]
+#         return self.items
+
+#     def remove(self, item):
+#         bits = mask(item)
+#         for index, bit in enumerate(bits):
+#             if bit == '1' and self.items[index]:
+#                 self.items[index] -= 1
---
+++ tests/test_counting_filter.py
@@ -0,0 +1,180 @@
+import sys
+sys.path.insert(1, '../')
+import unittest
+import random
+import math
+from src.data_structures.counting_filter import CountingFilter
+
+n = 1000        # Number of items we will put in our Bloom Filter
+p = 0.05        # Desired false positive rate
+buffer = 0.05   # Small buffer for false positive rate when unit testing
+
+
+class TestHashSet(unittest.TestCase):
+    
+    def test_test(self):
+        self.assertTrue(True)
+        self.assertFalse(False)
+
+    @classmethod
+    def get_false_positive_rate(self, numElements, numBits, numHash):
+        """
+        From class, we are given that for false positive rate follows the formula:
+        p = (1 - e^(-hn/m))^h
+        """
+        return (1 - math.exp(-numHash * numElements / numBits))**numHash
+
+    def test_insert_contains(self):
+        s = CountingFilter(n, p)
+        tempList = []
+        for i in range(1000):
+            tempList.append(i)
+
+        random.shuffle(tempList)
+        for i in tempList:
+            s.insert(str(i))
+
+        # If the Bloom Filter actually contains an element, it should always say it does
+        for i in range(1000):
+            self.assertTrue(s.contains(str(i)))
+
+        # If a Bloom Filter does not contain an element, there is a ~5% chance of a false positive
+        errors = 0
+        for i in range(-1, -1001, -1):
+            if s.contains(str(i)):
+                errors = errors + 1
+        print(errors)
+        self.assertTrue(errors < n * (p + buffer))
+
+    def test_union1(self):
+        a = CountingFilter(n, p)
+        b = CountingFilter(n, p)
+        for i in range(1000):
+            a.insert(str(i))
+
+        for i in range(1000, 2000):
+            b.insert(str(i))
+
+        a.union(b)
+
+        # If the union Bloom Filter actually contains an element, it should always say it does
+        for i in range(2000):
+            self.assertTrue(a.contains(str(i)))
+
+        # If a Bloom Filter does not contain an element, there is a chance of false positives, which will be calculated
+        # by the get_false_positive_rate method
+        errors = 0
+        for i in range(-1, -1001, -1):
+            if a.contains(str(i)):
+                errors = errors + 1
+        print("number of errors is" ,errors)
+        fpr = self.get_false_positive_rate(n * 2, a.getBitSize(), a.getHashCount())
+        print("false positive rate is ", fpr)
+        self.assertTrue(errors < n * (fpr + buffer))
+
+    def test_union2(self):
+        a = CountingFilter(n, p)
+        b = CountingFilter(n, p)
+        for i in range(1000):
+            a.insert(str(i))
+
+        for i in range(500, 1000):
+            b.insert(str(i))
+
+        a.union(b)
+
+        # If the union Bloom Filter actually contains an element, it should always say it does
+        for i in range(1000):
+            self.assertTrue(a.contains(str(i)))
+
+        # If a Bloom Filter does not contain an element, there is a chance of false positives
+        errors = 0
+        for i in range(-1, -1001, -1):
+            if a.contains(str(i)):
+                errors = errors + 1
+        print("number of errors is", errors)
+        fpr = self.get_false_positive_rate(n, a.getBitSize(), a.getHashCount())
+        print("false positive rate is", fpr)
+        self.assertTrue(errors < n * (fpr + buffer))
+
+    def test_intersection1(self):
+        a = CountingFilter(n, p / 50)  # we need to use a slightly smaller fpr here so we don't have too many intersects
+        b = CountingFilter(n, p / 50)
+        for i in range(1000):
+            a.insert(str(i))
+
+        for i in range(1000, 2000):
+            b.insert(str(i))
+
+        a.intersection(b)
+
+        # There are no elements in the intersection, so this bloom filter should contain nothing except for a few
+        # elements caused by hash collisions
+        errors = 0
+        for i in range(2000):
+            if a.contains(str(i)):
+                errors = errors + 1
+
+        print("number of intersections is", errors)
+        self.assertTrue(errors < n * 0.01)
+
+    def test_intersection2(self):
+        a = CountingFilter(n, p)
+        b = CountingFilter(n, p)
+        for i in range(1000):
+            a.insert(str(i))
+
+        for i in range(500, 1000):
+            b.insert(str(i))
+
+        a.intersection(b)
+
+        # There are no elements from 0 - 499, so in this range the bloom filter should only contain false postives
+        errors = 0
+        for i in range(500):
+            if a.contains(str(i)):
+                errors = errors + 1
+        print("intersect2 errors", errors)
+        fpr = self.get_false_positive_rate(500, a.getBitSize(), a.getHashCount())  # only 500 elements in intersect
+        print("intersect2 fpr", fpr)
+        self.assertTrue(errors < n * (fpr + buffer))
+
+        # These elements all should be in the intersection, so they must be in the intersect bloom filter
+        for i in range(500, 1000, 1):
+            self.assertTrue(a.contains(str(i)))
+
+    def test_intersection3(self):
+        a = CountingFilter(n, p)
+        b = CountingFilter(n, p)
+        c = CountingFilter(n, p)
+        for i in range(1000):
+            a.insert(str(i))
+
+        for i in range(500, 1000):
+            b.insert(str(i))
+
+        for i in range(600, 700):
+            c.insert(str(i))
+
+        b.intersection(c)
+        a.intersection(b)
+
+        fpr = self.get_false_positive_rate(100, a.getBitSize(), a.getHashCount())  # only 100 elements in intersect
+
+        # False positive errors should be expected in this range
+        errors = 0
+        for i in range(600):
+            if a.contains(str(i)):
+                errors = errors + 1
+        self.assertTrue(errors < n * (fpr + buffer))
+
+        # 100% accuracy is expected in this range
+        for i in range(600, 700):
+            self.assertTrue(a.contains(str(i)))
+
+        # False positive errors should be expected in this range
+        errors = 0
+        for i in range(700, 1000):
+            if a.contains(str(i)):
+                errors = errors + 1
+        self.assertTrue(errors < n * (fpr + buffer))
